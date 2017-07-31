e9712d777ce5949201236994af2eff231bbb6d3b
thoughflare <jykp2Vk5WY@local.host>
2017-07-31 03:03:44 +0000

    Add a shady foldable instance

--- src/Data/Hierarchy.purs
+++ src/Data/Hierarchy.purs
@@ -4,8 +4,11 @@ import Data.Argonaut.Decode (class DecodeJson)
 import Data.Argonaut.Decode.Generic (gDecodeJson)
 import Data.Argonaut.Encode (class EncodeJson)
 import Data.Argonaut.Encode.Generic (gEncodeJson)
+import Data.Array (head, init, last, tail)
+import Data.Foldable (class Foldable, foldMapDefaultL, foldl)
 import Data.Generic (class Generic, gShow)
-import Prelude (class Functor, class Show, map, ($))
+import Data.Maybe (Maybe(..))
+import Prelude (class Functor, class Show, append, flip, map, ($))
 
 data Hierarchy a = Cont a (Array (Hierarchy a))
 
@@ -21,3 +24,20 @@ instance showHierarchy :: (Generic a, Show a) => Show (Hierarchy a) where
 instance functorHierarchy :: Functor Hierarchy where
   map g (Cont x []) = Cont (g x) []
   map g (Cont x xs) = Cont (g x) $ map (map g) xs
+
+instance foldableHierarchy :: Foldable Hierarchy where
+  foldl g b f@(Cont x xs) = foldDo g b f (head xs) (tail xs)
+  foldr g b f@(Cont x xs) = foldDo (flip g) b f (last xs) (init xs)
+  foldMap = foldMapDefaultL
+
+foldDo :: forall a b .
+           (b -> a -> b) ->
+           b ->
+           Hierarchy a ->
+           Maybe (Hierarchy a) ->
+           Maybe (Array (Hierarchy a)) ->
+           b
+foldDo g b (Cont x xs) Nothing _  = g b x
+foldDo g b (Cont x xs) (Just (Cont x1 x1s)) (Just tl) =
+  foldl g (g b x) (Cont x1 (x1s `append` tl))
+foldDo _ b _ _ _ = b
--- src/Main.purs
+++ src/Main.purs
@@ -2,10 +2,11 @@ module Main where
 
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Prelude (Unit, show, ($), (<<<))
-
+import Data.Functor (map)
 import Data.Hierarchy (Hierarchy(..))
+import Data.String (length)
+import Prelude (Unit, show, ($), (<<<))
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
-  (log <<< show) $ Cont "Hello" [Cont "world" []]
+  (log <<< show <<< map length) $ Cont "Hello" [Cont "world!" []]
