946b6a42b0822a4e6cb10245bea0c52e81b049d5
thoughflare <jykp2Vk5WY@local.host>
2017-07-31 21:30:44 +0000

    Start getAt

--- src/Data/Hierarchy/Flat.purs
+++ src/Data/Hierarchy/Flat.purs
@@ -2,7 +2,11 @@ module Data.Hierarchy.Flat (mkFlat)
        where
 
 import Data.Hierarchy (Hierarchy(..))
+import Data.Maybe (Maybe(..))
 import Prelude (class Eq, flip)
 
 mkFlat :: forall a. Eq a => a -> Hierarchy a
 mkFlat = flip Cont []
+
+getAt :: forall a. Eq a => Hierarchy a -> a -> Maybe (Hierarchy a)
+getAt h _ = Just h
