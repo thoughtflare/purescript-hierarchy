22e49a85fe9e17fc873c9d4ebd61a97a8fe6c93d
thoughflare <jykp2Vk5WY@local.host>
2017-08-03 20:39:09 +0000

    Make foldDo into be more general, rename to foldTill

--- src/Data/Hierarchy.purs
+++ src/Data/Hierarchy.purs
@@ -8,7 +8,8 @@ import Data.Array (head, init, last, tail)
 import Data.Foldable (class Foldable, foldMapDefaultL, foldl)
 import Data.Generic (class Generic, gShow)
 import Data.Maybe (Maybe(..))
-import Prelude (class Functor, class Show, append, flip, map, ($))
+import Data.Tuple (Tuple(..), fst, snd)
+import Prelude (class Functor, class Show, append, flip, map, ($), (<<<))
 
 data Hierarchy a = Cont a (Array (Hierarchy a))
 
@@ -26,19 +27,25 @@ instance functorHierarchy :: Functor Hierarchy where
   map g (Cont x xs) = Cont (g x) $ map (map g) xs
 
 instance foldableHierarchy :: Foldable Hierarchy where
-  foldl g b f@(Cont x xs) = foldDo g b f (head xs) (tail xs)
-  foldr g b f@(Cont x xs) = foldDo (flip g) b f (last xs) (init xs)
+  foldl g b f@(Cont x xs) = fst $ foldTill (constFst true g)
+                                           (Tuple true b) f (head xs) (tail xs)
+  foldr g b f@(Cont x xs) = fst $ foldTill (constFst true (flip g))
+                                           (Tuple true b) f (last xs) (init xs)
   foldMap = foldMapDefaultL
 
-foldDo :: forall a b .
-           (b -> a -> b) ->
-           b ->
+constFst :: forall a b c . a -> (c -> b -> c) -> (Tuple a c -> b -> Tuple a c)
+constFst x f y z = Tuple x (f (snd y) z)
+
+foldTill :: forall a b .
+           (Tuple Boolean b -> a -> Tuple Boolean b) ->
+           Tuple Boolean b ->
            Hierarchy a ->
            Maybe (Hierarchy a) ->
            Maybe (Array (Hierarchy a)) ->
-           b
-foldDo g b (Cont x xs) Nothing _  = g b x
-foldDo g b (Cont x xs) (Just (Cont x1 x1s)) (Just tl) =
-  foldl g (g b x) (Cont x1 (x1s `append` tl))
-foldDo g b (Cont x xs) (Just f1) Nothing =
-  foldl g (g b x) f1
+           Tuple b (Maybe (Hierarchy a))
+foldTill _ (Tuple false acc) f _ _ = Tuple acc (Just f)
+foldTill g b (Cont x xs) Nothing _ = Tuple (snd $ g b x) Nothing
+foldTill g b (Cont x xs) (Just (Cont x1 x1s)) (Just tl) =
+  foldTill g (g b x) (Cont x1 (x1s `append` tl)) (head (x1s `append` tl)) (tail (x1s `append` tl))
+foldTill g b (Cont x xs) (Just (Cont x1 x1s)) Nothing =
+  foldTill g (g b x) (Cont x1 x1s) (head x1s) (tail x1s)
--- src/Data/Hierarchy/Flat.purs
+++ src/Data/Hierarchy/Flat.purs
@@ -1,12 +1,20 @@
 module Data.Hierarchy.Flat (mkFlat)
        where
 
+import Data.Foldable (foldl)
 import Data.Hierarchy (Hierarchy(..))
 import Data.Maybe (Maybe(..))
-import Prelude (class Eq, flip)
+import Prelude (class Eq, flip, otherwise, (==))
 
 mkFlat :: forall a. Eq a => a -> Hierarchy a
 mkFlat = flip Cont []
 
-getAt :: forall a. Eq a => Hierarchy a -> a -> Maybe (Hierarchy a)
-getAt h _ = Just h
+{--
+at :: forall a. Eq a => Hierarchy a -> a -> Maybe (Hierarchy a)
+at h x = foldl getDo Nothing h
+  where
+    getDo r@(Just _) _ = r
+    getDo Nothing e
+      | x == e    = Just e
+      | otherwise = Nothing
+--}
